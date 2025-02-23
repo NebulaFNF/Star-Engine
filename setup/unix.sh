91dab282f744a8adf2a20f32b8bcae667b8655a4
Lunar <144155299+SyncGit12@users.noreply.github.com>
2025-02-23 18:25:59 -0300

    funny

--- setup/unix.sh
+++ setup/unix.sh
@@ -9,9 +9,9 @@ haxelib install discord_rpc 1.0.0
 haxelib install flixel-addons 2.11.0
 haxelib install flixel-demos 2.9.0
 haxelib install flixel-ui 2.4.0
-haxelib install flixel 4.11.0
+haxelib install flixel 5.2.2
 haxelib install flxanimate 3.0.4
-haxelib git hxCodec https://github.com/SyncGit12/hxCodec
+haxelib install hxCodec 2.5.1
 haxelib install openfl 9.3.0
 haxelib install lime
 haxelib run lime setup flixel
@@ -27,4 +27,5 @@ haxelib set openfl 9.3.0
 haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git hxCodec https://github.com/SyncGit12/hxCodec
 echo Finished!
