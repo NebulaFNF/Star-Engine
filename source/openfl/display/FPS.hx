1cd768e0c194192ec7d9b231fa31d5b875ea5872
Lunar <144155299+SyncGit12@users.noreply.github.com>
2025-03-13 09:47:32 -0300

    Update FPS.hx

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -103,7 +103,7 @@ class FPS extends TextField
 			#end
 
 			textColor = 0xFFFFFFFF;
-			if (memoryMegas > 3072 || currentFPS <= ClientPrefs.framerate / 2)
+			if (memoryShit > 3072 || currentFPS <= ClientPrefs.framerate / 2)
 			{
 				textColor = 0xFFFF0000;
 			}
