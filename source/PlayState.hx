e4a258d8fbd68a812f0b71f36c7e32abc5a6d500
Lunar <144155299+SyncGit12@users.noreply.github.com>
2025-03-17 10:52:11 -0300

    Fixing Philly Streets Stage

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -886,13 +886,13 @@ class PlayState extends MusicBeatState
 
 			// WHY THE FUCK IS THE STAGES HARDCODED??
 		    case 'phillyStreets':
-				var phillyWhole:BGSprite = new BGSprite('phillyStreets/streetsWhole', -1300, -700, 0.9, 0.9);
+				var phillyWhole:BGSprite = new BGSprite('phillyStreets/streetsWhole', -2000, -700, 0.9, 0.9);
 				phillyWhole.setGraphicSize(Std.int(phillyWhole.width * 0.5));
 				phillyWhole.antialiasing = false;
 				add(phillyWhole);
 
 				if(!ClientPrefs.lowQuality) {
-					var phillyWhole:BGSprite = new BGSprite('phillyStreets/streetsWhole', -1300, -700, 0.9, 0.9);
+					var phillyWhole:BGSprite = new BGSprite('phillyStreets/streetsWhole', -1500, -700, 0.9, 0.9);
 					phillyWhole.setGraphicSize(Std.int(phillyWhole.width * 0.5));
 					phillyWhole.antialiasing = true;
 				    add(phillyWhole);
