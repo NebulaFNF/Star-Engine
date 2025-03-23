89beb0acf9048825676fccab532c02b611db09a0
Cyklus <100812875+cyklusiguess@users.noreply.github.com>
2025-03-23 19:54:29 -0300

    updated credits.hx + little rewrites?
    idk if i fucked something up so lmk

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -20,289 +20,186 @@ using StringTools;
 
 class CreditsState extends MusicBeatState
 {
-	var curSelected:Int = -1;
+    private var curSelected:Int = -1;
+    private var grpOptions:FlxTypedGroup<Alphabet>;
+    private var iconArray:Array<AttachedSprite> = [];
+    private var creditsData:Array<Array<String>> = [];
 
-	private var grpOptions:FlxTypedGroup<Alphabet>;
-	private var iconArray:Array<AttachedSprite> = [];
-	private var creditsStuff:Array<Array<String>> = [];
+    private var bg:FlxSprite;
+    private var descText:FlxText;
+    private var intendedColor:Int;
+    private var colorTween:FlxTween;
+    private var descBox:AttachedSprite;
 
-	var bg:FlxSprite;
-	var descText:FlxText;
-	var intendedColor:Int;
-	var colorTween:FlxTween;
-	var descBox:AttachedSprite;
+    private var offsetY:Float = -75;
+    private var holdTime:Float = 0;
+    private var quitting:Bool = false;
+    private var moveTween:FlxTween = null;
 
-	var offsetThing:Float = -75;
+    override function create()
+    {
+        #if desktop
+        DiscordClient.changePresence("Browsing Credits", null);
+        #end
 
-	override function create()
-	{
-		#if desktop
-		// Updating Discord Rich Presence
-		DiscordClient.changePresence("In the Menus", null);
-		#end
+        persistentUpdate = true;
+        
+        bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+        bg.screenCenter();
+        add(bg);
+        
+        grpOptions = new FlxTypedGroup<Alphabet>();
+        add(grpOptions);
 
-		persistentUpdate = true;
-		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
-		add(bg);
-		bg.screenCenter();
-		
-		grpOptions = new FlxTypedGroup<Alphabet>();
-		add(grpOptions);
+        populateCredits();
+        initializeTextElements();
 
-		#if MODS_ALLOWED
-		var path:String = 'modsList.txt';
-		if(FileSystem.exists(path))
-		{
-			var leMods:Array<String> = CoolUtil.coolTextFile(path);
-			for (i in 0...leMods.length)
-			{
-				if(leMods.length > 1 && leMods[0].length > 0) {
-					var modSplit:Array<String> = leMods[i].split('|');
-					if(!Paths.ignoreModFolders.contains(modSplit[0].toLowerCase()) && !modsAdded.contains(modSplit[0]))
-					{
-						if(modSplit[1] == '1')
-							pushModCreditsToList(modSplit[0]);
-						else
-							modsAdded.push(modSplit[0]);
-					}
-				}
-			}
-		}
+        bg.color = getCurrentBGColor();
+        intendedColor = bg.color;
+        changeSelection();
 
-		var arrayOfFolders:Array<String> = Paths.getModDirectories();
-		arrayOfFolders.push('');
-		for (folder in arrayOfFolders) pushModCreditsToList(folder);
-		#end
+        super.create();
+    }
 
-		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
-			['Star Engine Team'],
-			['SyncGit12', 'star', 'Owner of Star Engine', 'https://github.com/SyncGit12', '696900'],
-			['Stinko', 'stinkern', 'Small debugger, Coder, a pretty dumb one...', 'https://stinkernn.carrd.co', '696969'],
-			['Nael2xd', 'nael', 'Programmer', 'https://www.youtube.com/@Nael2xd', '880000'],
-			
-			[''],
-			['Special Thanks'],
-			['HRK. EXEX',			'hrk',				'Created Video Rendering Mode for FNF',								'https://www.youtube.com/@HRK_EXEX/featured',			'01F8FF'],
-			['Stefan2008',			'stefan',				'The tips on the main menu!',								'https://www.youtube.com/@stefan2008official',			'8B4513'],
+    override function update(elapsed:Float)
+    {
+        if (FlxG.sound.music.volume < 0.7)
+            FlxG.sound.music.volume += 0.5 * elapsed;
 
-			[''],
-			
-			['Psych Engine Team'],
-			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',								'https://twitter.com/Shadow_Mario_',	'444444'],
-			['RiverOaken',			'river',			'Main Artist/Animator of Psych Engine',							'https://twitter.com/RiverOaken',		'B42F71'],
-			['shubs',				'shubs',			'Additional Programmer of Psych Engine',						'https://twitter.com/yoshubs',			'5E99DF'],
-			
-			[''],
-			
-			['Former Engine Members'],
-			['bb-panzu',			'bb',				'Ex-Programmer of Psych Engine',								'https://twitter.com/bbsub3',			'3E813A'],
-			
-			[''],
-			
-			['Engine Contributors'],
-			['iFlicky',				'flicky',			'Composer of Psync and Tea Time\nMade the Dialogue Sounds',		'https://twitter.com/flicky_i',			'9E29CF'],
-			['SqirraRNG',			'sqirra',			'Crash Handler and Base code for\nChart Editor\'s Waveform',	'https://twitter.com/gedehari',			'E1843A'],
-			['EliteMasterEric',		'mastereric',		'Runtime Shaders support',										'https://twitter.com/EliteMasterEric',	'FFBD40'],
-			['PolybiusProxy',		'proxy',			'.MP4 Video Loader Library (hxCodec)',							'https://twitter.com/polybiusproxy',	'DCD294'],
-			['KadeDev',				'kade',				'Fixed some cool stuff on Chart Editor\nand other PRs',			'https://twitter.com/kade0912',			'64A250'],
-			['Keoiki',				'keoiki',			'Note Splash Animations',										'https://twitter.com/Keoiki_',			'D2D2D2'],
-			['Nebula the Zorua',	'nebula',			'LUA JIT Fork and some Lua reworks',							'https://twitter.com/Nebula_Zorua',		'7D40B2'],
-			['Smokey',				'smokey',			'Sprite Atlas Support',											'https://twitter.com/Smokey_5_',		'483D92'],
-			
-			[''],
-			
-			["Funkin' Crew"],
-			['ninjamuffin99',		'ninjamuffin99',	"Programmer of Friday Night Funkin'",							'https://twitter.com/ninja_muffin99',	'CF2D2D'],
-			['PhantomArcade',		'phantomarcade',	"Animator of Friday Night Funkin'",								'https://twitter.com/PhantomArcade3K',	'FADC45'],
-			['evilsk8r',			'evilsk8r',			"Artist of Friday Night Funkin'",								'https://twitter.com/evilsk8r',			'5ABD4B'],
-			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",								'https://twitter.com/kawaisprite',		'378FC7']
-		];
-		
-		for(i in pisspoop) creditsStuff.push(i);
-	
-		for (i in 0...creditsStuff.length)
-		{
-			var isSelectable:Bool = !unselectableCheck(i);
-			var optionText:Alphabet = new Alphabet(FlxG.width / 2, 300, creditsStuff[i][0], !isSelectable);
-			optionText.isMenuItem = true;
-			optionText.targetY = i;
-			optionText.changeX = false;
-			optionText.snapToPosition();
-			grpOptions.add(optionText);
+        if (!quitting)
+        {
+            handleInput(elapsed);
+            updateTextAlignment(elapsed);
+        }
 
-			if(isSelectable) {
-				if(creditsStuff[i][5] != null) Paths.currentModDirectory = creditsStuff[i][5];
+        super.update(elapsed);
+    }
 
-				var icon:AttachedSprite = new AttachedSprite('credits/' + creditsStuff[i][1]);
-				icon.xAdd = optionText.width + 10;
-				icon.sprTracker = optionText;
-	
-				// using a FlxGroup is too much fuss!
-				iconArray.push(icon);
-				add(icon);
-				Paths.currentModDirectory = '';
+private function populateCredits()
+{
+    var creditsList:Array<Array<String>> = [
+        ['Star Engine Team'],
+        ['SyncGit12', 'star', 'Lead Developer', 'https://github.com/SyncGit12', '696900'],
+        ['Stinko', 'stinkern', 'Debugger & Coder', 'https://stinkernn.carrd.co', '696969'],
+        ['Nael2xd', 'nael', 'Programmer', 'https://www.youtube.com/@Nael2xd', '880000'],
+        ['lunaruniv69', 'lunaruniv69', 'Lead Developer, Logo Designer', '', 'FF5733'],
+        ['moxie-coder', 'moxie-coder', 'Programmer', '', 'C70039'],
+        ['mcagabe19', 'mcagabe19', 'Mobile Port Developer, Programmer', '', '900C3F'],
+        ['cyklusiguess', 'cyklusiguess', 'Developer', '', '581845'],
+        ['HRK_EXEX', 'hrk', 'Developer, FPS Counter Creator, Rendering Mode Creator', 'https://www.youtube.com/@HRK_EXEX/featured', '01F8FF'],
+        ['TheStinkern', 'thestinkern', 'Small coder, debugger', '', '6A0DAD'],
+        
+        ['Special Thanks'],
+        ['Stefan2008', 'stefan', 'Main Menu Tips Contributor', 'https://www.youtube.com/@stefan2008official', '8B4513']
+    ];
+    
+    for (entry in creditsList)
+        creditsData.push(entry);
 
-				if(curSelected == -1) curSelected = i;
-			}
-			else optionText.alignment = CENTERED;
-		}
-		
-		descBox = new AttachedSprite();
-		descBox.makeGraphic(1, 1, FlxColor.BLACK);
-		descBox.xAdd = -10;
-		descBox.yAdd = -10;
-		descBox.alphaMult = 0.6;
-		descBox.alpha = 0.6;
-		add(descBox);
+    for (i in 0...creditsData.length)
+    {
+        var selectable:Bool = !isUnselectable(i);
+        var optionText:Alphabet = new Alphabet(FlxG.width / 2, 300, creditsData[i][0], !selectable);
+        optionText.isMenuItem = true;
+        optionText.targetY = i;
+        optionText.changeX = false;
+        optionText.snapToPosition();
+        grpOptions.add(optionText);
 
-		descText = new FlxText(50, FlxG.height + offsetThing - 25, 1180, "", 32);
-		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER/*, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK*/);
-		descText.scrollFactor.set();
-		//descText.borderSize = 2.4;
-		descBox.sprTracker = descText;
-		add(descText);
+        if (selectable)
+        {
+            var icon:AttachedSprite = new AttachedSprite('credits/' + creditsData[i][1]);
+            icon.xAdd = optionText.width + 10;
+            icon.sprTracker = optionText;
+            iconArray.push(icon);
+            add(icon);
+            if (curSelected == -1) curSelected = i;
+        }
+        else
+        {
+            optionText.alignment = CENTERED;
+        }
+    }
+}
 
-		bg.color = getCurrentBGColor();
-		intendedColor = bg.color;
-		changeSelection();
-		super.create();
-	}
 
-	var quitting:Bool = false;
-	var holdTime:Float = 0;
-	override function update(elapsed:Float)
-	{
-		if (FlxG.sound.music.volume < 0.7) FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
+    private function initializeTextElements()
+    {
+        descBox = new AttachedSprite();
+        descBox.makeGraphic(1, 1, FlxColor.BLACK);
+        descBox.alphaMult = 0.6;
+        descBox.alpha = 0.6;
+        add(descBox);
 
-		if(!quitting)
-		{
-			if(creditsStuff.length > 1)
-			{
-				var shiftMult:Int = 1;
-				if(FlxG.keys.pressed.SHIFT) shiftMult = 3;
+        descText = new FlxText(50, FlxG.height + offsetY - 25, 1180, "", 32);
+        descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER);
+        descText.scrollFactor.set();
+        descBox.sprTracker = descText;
+        add(descText);
+    }
 
-				var upP = controls.UI_UP_P;
-				var downP = controls.UI_DOWN_P;
+    private function handleInput(elapsed:Float)
+    {
+        var shiftMult:Int = (FlxG.keys.pressed.SHIFT) ? 3 : 1;
+        var upPressed = controls.UI_UP_P;
+        var downPressed = controls.UI_DOWN_P;
 
-				if (upP) {
-					changeSelection(-shiftMult);
-					holdTime = 0;
-				} if (downP) {
-					changeSelection(shiftMult);
-					holdTime = 0;
-				}
+        if (upPressed) changeSelection(-shiftMult);
+        if (downPressed) changeSelection(shiftMult);
 
-				if(controls.UI_DOWN || controls.UI_UP)
-				{
-					var checkLastHold:Int = Math.floor((holdTime - 0.5) * 10);
-					holdTime += elapsed;
-					var checkNewHold:Int = Math.floor((holdTime - 0.5) * 10);
+        if (controls.UI_DOWN || controls.UI_UP)
+        {
+            var prevHoldTime:Int = Math.floor((holdTime - 0.5) * 10);
+            holdTime += elapsed;
+            var newHoldTime:Int = Math.floor((holdTime - 0.5) * 10);
 
-					if(holdTime > 0.5 && checkNewHold - checkLastHold > 0) changeSelection((checkNewHold - checkLastHold) * (controls.UI_UP ? -shiftMult : shiftMult));
-				}
-			}
+            if (holdTime > 0.5 && newHoldTime > prevHoldTime)
+                changeSelection((newHoldTime - prevHoldTime) * (controls.UI_UP ? -shiftMult : shiftMult));
+        }
 
-			if(controls.ACCEPT && (creditsStuff[curSelected][3] == null || creditsStuff[curSelected][3].length > 4)) CoolUtil.browserLoad(creditsStuff[curSelected][3]);
-			if (controls.BACK)
-			{
-				if(colorTween != null) colorTween.cancel();
-				FlxG.sound.play(Paths.sound('cancelMenu'));
-				MusicBeatState.switchState(new MainMenuState());
-				quitting = true;
-			}
-		}
-		
-		for (item in grpOptions.members)
-		{
-			if(!item.bold)
-			{
-				var lerpVal:Float = CoolUtil.boundTo(elapsed * 12, 0, 1);
-				if(item.targetY == 0)
-				{
-					var lastX:Float = item.x;
-					item.screenCenter(X);
-					item.x = FlxMath.lerp(lastX, item.x - 70, lerpVal);
-				}
-				else item.x = FlxMath.lerp(item.x, 200 + -40 * Math.abs(item.targetY), lerpVal);
-			}
-		}
-		super.update(elapsed);
-	}
+        if (controls.ACCEPT && creditsData[curSelected][3] != null)
+            CoolUtil.browserLoad(creditsData[curSelected][3]);
 
-	var moveTween:FlxTween = null;
-	function changeSelection(change:Int = 0)
-	{
-		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-		do {
-			curSelected += change;
-			if (curSelected < 0) curSelected = creditsStuff.length - 1;
-			if (curSelected >= creditsStuff.length) curSelected = 0;
-		} while(unselectableCheck(curSelected));
+        if (controls.BACK)
+        {
+            if (colorTween != null) colorTween.cancel();
+            FlxG.sound.play(Paths.sound('cancelMenu'));
+            MusicBeatState.switchState(new MainMenuState());
+            quitting = true;
+        }
+    }
 
-		var newColor:Int =  getCurrentBGColor();
-		if(newColor != intendedColor) {
-			if(colorTween != null) colorTween.cancel();
-			intendedColor = newColor;
-			colorTween = FlxTween.color(bg, 1, bg.color, intendedColor, {
-				onComplete: function(twn:FlxTween) {
-					colorTween = null;
-				}
-			});
-		}
+    private function updateTextAlignment(elapsed:Float)
+    {
+        for (item in grpOptions.members)
+        {
+            var lerpFactor:Float = CoolUtil.boundTo(elapsed * 12, 0, 1);
+            item.x = (item.targetY == 0) ? FlxMath.lerp(item.x, FlxG.width / 2 - 70, lerpFactor) : FlxMath.lerp(item.x, 200 + -40 * Math.abs(item.targetY), lerpFactor);
+        }
+    }
 
-		var bullShit:Int = 0;
+    private function changeSelection(change:Int = 0)
+    {
+        do {
+            curSelected += change;
+            if (curSelected < 0) curSelected = creditsData.length - 1;
+            if (curSelected >= creditsData.length) curSelected = 0;
+        } while (isUnselectable(curSelected));
 
-		for (item in grpOptions.members)
-		{
-			item.targetY = bullShit - curSelected;
-			bullShit++;
+        descText.text = creditsData[curSelected][2];
+        descText.y = FlxG.height - descText.height + offsetY - 60;
 
-			if(!unselectableCheck(bullShit-1)) {
-				item.alpha = 0.6;
-				if (item.targetY == 0) item.alpha = 1;
-			}
-		}
+        if (moveTween != null) moveTween.cancel();
+        moveTween = FlxTween.tween(descText, {y: descText.y + 75}, 0.25, {ease: FlxEase.sineOut});
+    }
 
-		descText.text = creditsStuff[curSelected][2];
-		descText.y = FlxG.height - descText.height + offsetThing - 60;
+    private function getCurrentBGColor():Int
+    {
+        return Std.parseInt("0xFF" + creditsData[curSelected][4]);
+    }
 
-		if(moveTween != null) moveTween.cancel();
-		moveTween = FlxTween.tween(descText, {y : descText.y + 75}, 0.25, {ease: FlxEase.sineOut});
-
-		descBox.setGraphicSize(Std.int(descText.width + 20), Std.int(descText.height + 25));
-		descBox.updateHitbox();
-	}
-
-	#if MODS_ALLOWED
-	private var modsAdded:Array<String> = [];
-	function pushModCreditsToList(folder:String)
-	{
-		if(modsAdded.contains(folder)) return;
-
-		var creditsFile:String = null;
-		if(folder != null && folder.trim().length > 0) creditsFile = Paths.mods(folder + '/data/credits.txt');
-		else creditsFile = Paths.mods('data/credits.txt');
-
-		if (FileSystem.exists(creditsFile))
-		{
-			var firstarray:Array<String> = File.getContent(creditsFile).split('\n');
-			for(i in firstarray)
-			{
-				var arr:Array<String> = i.replace('\\n', '\n').split("::");
-				if(arr.length >= 5) arr.push(folder);
-				creditsStuff.push(arr);
-			}
-			creditsStuff.push(['']);
-		}
-		modsAdded.push(folder);
-	}
-	#end
-
-	function getCurrentBGColor() {
-		var bgColor:String = creditsStuff[curSelected][4];
-		if(!bgColor.startsWith('0x')) bgColor = '0xFF' + bgColor;
-		return Std.parseInt(bgColor);
-	}
-
-	private function unselectableCheck(num:Int):Bool return creditsStuff[num].length <= 1;
+    private function isUnselectable(index:Int):Bool
+    {
+        return creditsData[index].length <= 1;
+    }
 }
