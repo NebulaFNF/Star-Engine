58181f99322c99bca1dbc3ee0b0952978de99b88
Lunar <144155299+SyncGit12@users.noreply.github.com>
2025-04-13 19:28:59 -0300

    tried something

--- setup/windows.bat
+++ setup/windows.bat
@@ -49,6 +49,7 @@ haxelib git haxeui-flixel https://github.com/haxeui/haxeui-flixel da27e833947f32
 haxelib git haxeui-core https://github.com/haxeui/haxeui-core 51c23588614397089a5ce182cddea729f0be6fa0
 haxelib git flixel-text-input https://github.com/FunkinCrew/flixel-text-input 951a0103a17bfa55eed86703ce50b4fb0d7590bc
 haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp master
+haxelib git crashdumper http://github.com/larsiusprime/crashdumper
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
 haxelib set flixel 5.2.2
--- source/Main.hx
+++ source/Main.hx
@@ -4,8 +4,6 @@ import AnsiTrace;
 import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxState;
-import flixel.graphics.FlxGraphic;
-import openfl.Assets;
 import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.FPSBg;
@@ -14,23 +12,19 @@ import openfl.display.StageScaleMode;
 import openfl.events.Event;
 
 using StringTools;
+
+/*#if debug
+	import crashdumper.*;
+	#end */
 // credits to hrk ex ex !!!!
 #if desktop
 import backend.ALSoftConfig; // Just to make sure DCE doesn't remove this, since it's not directly referenced anywhere else.
 #end
-
-//crash handler stuff
+// crash handler stuff
 #if CRASH_HANDLER
-import Discord.DiscordClient;
 import haxe.CallStack;
-import haxe.io.Path;
 import lime.app.Application;
-import openfl.events.UncaughtErrorEvent;
-import sys.FileSystem;
-import sys.io.File;
-import sys.io.Process;
 #end
-
 
 class Main extends Sprite
 {
@@ -42,64 +36,65 @@ class Main extends Sprite
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 	var curVersion:String = "";
+
 	public static var fpsVar:FPS;
 	public static var fpsBg:FPSBg;
 
 	public static var luversion:String = '0.0.1';
 
 	public static final __superCoolErrorMessagesArray:Array<String> = [
-        "A fatal error has occ- wait what?",
-        "missigno.",
-        "oopsie daisies!! you did a fucky wucky!!",
-        "i think you fogot a semicolon",
-        "null balls reference",
-        "get friday night funkd'",
-        "engine skipped a heartbeat",
-        "Impossible...",
-        "Patience is key for success... Don't give up.",
-        "It's no longer in its early stages... is it?",
-        "It took me half a day to code that in",
-        "You should make an issue... NOW!!",
-        "> Crash Handler written by: yoshicrafter29",
-        "broken ch-... wait what are we talking about",
-        "could not access variable you.dad",
-        "What have you done...",
-        "THERE ARENT COUGARS IN SCRIPTING!!! I HEARD IT!!",
-        "no, thats not from system.windows.forms",
-        "you better link a screenshot if you make an issue, or at least the crash.txt",
-    	"stack trace more like dunno i dont have any jokes",
-        "oh the misery. everybody wants to be my enemy",
-        "have you heard of soulles dx",
-        "i thought it was invincible",
-        "did you deleted coconut.png",
-        "have you heard of missing json's cousin null function reference",
-        "sad that linux users wont see this banger of a crash handler",
+		"A fatal error has occ- wait what?",
+		"missigno.",
+		"oopsie daisies!! you did a fucky wucky!!",
+		"i think you fogot a semicolon",
+		"null balls reference",
+		"get friday night funkd'",
+		"engine skipped a heartbeat",
+		"Impossible...",
+		"Patience is key for success... Don't give up.",
+		"It's no longer in its early stages... is it?",
+		"It took me half a day to code that in",
+		"You should make an issue... NOW!!",
+		"> Crash Handler written by: yoshicrafter29",
+		"broken ch-... wait what are we talking about",
+		"could not access variable you.dad",
+		"What have you done...",
+		"THERE ARENT COUGARS IN SCRIPTING!!! I HEARD IT!!",
+		"no, thats not from system.windows.forms",
+		"you better link a screenshot if you make an issue, or at least the crash.txt",
+		"stack trace more like dunno i dont have any jokes",
+		"oh the misery. everybody wants to be my enemy",
+		"have you heard of soulles dx",
+		"i thought it was invincible",
+		"did you deleted coconut.png",
+		"have you heard of missing json's cousin null function reference",
+		"sad that linux users wont see this banger of a crash handler",
 		"woopsie",
-        "oopsie",
-        "woops",
-        "silly me",
-        "my bad",
-        "first time, huh?",
-        "did somebody say yoga",
-        "we forget a thousand things everyday... make sure this is one of them.",
-        "SAY GOODBYE TO YOUR KNEECAPS, CHUCKLEHEAD",
-        "motherfucking ordinal 344 (TaskDialog) forcing me to create a even fancier window",
-        "Died due to missing a sawblade. (Press Space to dodge!)",
-        "yes rico, kaboom.",
-        "hey, while in freeplay, press shift while pressing space",
-        "goofy ahh engine",
-        "pssst, try typing debug7 in the options menu",
-        "this crash handler is sponsored by rai-",
-        "",
-        "did you know a jiffy is an actual measurement of time",
-        "how many hurt notes did you put",
-        "FPS: 0",
-        "\r\ni am a secret message",
-        "this is garnet",
-        "Error: Sorry i already have a girlfriend",
-        "did you know theres a total of 51 silly messages",
-        "whoopsies looks like i forgot to fix this",
-        "Game used Crash. It's super effective!",
+		"oopsie",
+		"woops",
+		"silly me",
+		"my bad",
+		"first time, huh?",
+		"did somebody say yoga",
+		"we forget a thousand things everyday... make sure this is one of them.",
+		"SAY GOODBYE TO YOUR KNEECAPS, CHUCKLEHEAD",
+		"motherfucking ordinal 344 (TaskDialog) forcing me to create a even fancier window",
+		"Died due to missing a sawblade. (Press Space to dodge!)",
+		"yes rico, kaboom.",
+		"hey, while in freeplay, press shift while pressing space",
+		"goofy ahh engine",
+		"pssst, try typing debug7 in the options menu",
+		"this crash handler is sponsored by rai-",
+		"",
+		"did you know a jiffy is an actual measurement of time",
+		"how many hurt notes did you put",
+		"FPS: 0",
+		"\r\ni am a secret message",
+		"this is garnet",
+		"Error: Sorry i already have a girlfriend",
+		"did you know theres a total of 51 silly messages",
+		"whoopsies looks like i forgot to fix this",
+		"Game used Crash. It's super effective!",
 		"What in the fucking shit fuck dick!",
 		"The engine got constipated. Sad.",
 		"shit.",
@@ -108,7 +103,7 @@ class Main extends Sprite
 		"uhhhhhhhhhhhhhhhh... i dont think this is normal...",
 		"lobotomy moment",
 		"ARK: Survival Evolved"
-    ];
+	];
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
@@ -127,7 +122,7 @@ class Main extends Sprite
 		#end
 
 		CrashHandler.init();
-	
+
 		if (stage != null)
 		{
 			init();
@@ -138,8 +133,9 @@ class Main extends Sprite
 		}
 	}
 
-	public static function changeWindowName(name:String = "") {
-		Application.current.window.title = 'Star Engine - LuApps v$luversion ${name != "" ? '- $name' : ''}';
+	public static function changeWindowName(name:String = "")
+	{
+		Application.current.window.title = "Friday Night Funkin'" + ":" + "Star Engine - LuApps v" + luversion + '${name != "" ? '- $name' : ''}';
 	}
 
 	private function init(?E:Event):Void
@@ -159,13 +155,13 @@ class Main extends Sprite
 		#else
 		trace('hxcpp_debug_server is disabled! This build does not support debugging.');
 		#end
-		
+
 		#if windows
 		WindowColorMode.setDarkMode();
 		if (CoolUtil.hasVersion("Windows 10"))
 			WindowColorMode.redrawWindowHeader();
 		#end
-	
+
 		var stageWidth:Int = Lib.current.stage.stageWidth;
 		var stageHeight:Int = Lib.current.stage.stageHeight;
 
@@ -177,7 +173,7 @@ class Main extends Sprite
 			gameWidth = Math.ceil(stageWidth / zoom);
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
-	
+
 		ClientPrefs.loadDefaultKeys();
 		inline cpp.vm.Gc.enable(!ClientPrefs.disableGC);
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
@@ -189,7 +185,8 @@ class Main extends Sprite
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
-		if(fpsVar != null) {
+		if (fpsVar != null)
+		{
 			fpsVar.visible = ClientPrefs.showFPS;
 			fpsBg.visible = ClientPrefs.showFPS;
 		}
